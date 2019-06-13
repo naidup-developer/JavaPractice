6cb2c8e571c3f7901d9afdbdb9bcd958c42b27d8
naidu <you@example.comnaidup.developer@gmail.com>
2019-06-13 11:13:18 +0530

    adding addition and OddEven Check

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Addition
+{
+	 public static void main(String...args) 
+	 {
+
+	 	Scanner sc= new Scanner(System.in);
+	 	System.out.println("Enter First No :");
+	 	int num1=sc.nextInt();
+	 	System.out.println("Enter Second No:");
+	 	int num2=sc.nextInt();
+
+	 	System.out.println("Total :"+(num1+num2));
+		
+	}
+}
---
+++ OddEvenCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class OddEvenCheck
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+
+		int number=sc.nextInt();
+
+		if ((number%2)==0) 
+		{
+			System.out.println(number+" is Even Number");
+		}else{
+			System.out.println(number+" is Odd Number");
+		}
+	}
+}
